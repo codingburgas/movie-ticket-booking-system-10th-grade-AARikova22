003130976727544f52aa2f55f76fd3ef30e1aae8
AARikova22 <AARikova22@codingburgas.bg>
2025-06-29 18:11:14 +0300

    fix searching functions

--- src/cinema.cpp
+++ src/cinema.cpp
@@ -17,104 +17,113 @@ void Cinema::printallshowsInHalls() {
 }
 
 vector<SearchResult> Cinema::SearchShowbytitle(string usertitle) {
-    vector<SearchResult> results;
-
+    vector<SearchResult> result;
     string lower_usertitle = usertitle;
-    for (int i = 0; i < usertitle.size(); i++) {
-        lower_usertitle[i] = tolower(usertitle[i]);
+    for (char& c : lower_usertitle) {
+        c = tolower(c);
     }
 
     for (int i = 0; i < halls.size(); i++) {
         for (int j = 0; j < halls[i].shows.size(); j++) {
-            string lower_movie_title = halls[i].shows[j].movie->title;
-            for (int k = 0; k < halls[i].shows[j].movie->title.size(); k++) {
-                lower_movie_title[k] = tolower(halls[i].shows[j].movie->title[k]);
-            }
+            if (halls[i].shows[j].movie->title != "_DELETED_") {
+                string lower_movie_title = halls[i].shows[j].movie->title;
+                for (char& c : lower_movie_title) {
+                    c = tolower(c);
+                }
 
-            if (lower_movie_title == lower_usertitle) {
-                SearchResult sr;
-                sr.show = &halls[i].shows[j];
-                sr.hallNumber = halls[i].HallNumber;
-                results.push_back(sr);
+                if (lower_movie_title == lower_usertitle) {
+                    SearchResult sr;
+                    sr.show = &halls[i].shows[j];
+                    sr.hallNumber = halls[i].HallNumber;
+                    result.push_back(sr);
+                }
             }
         }
     }
-    return results;
+    return result;
 }
-
 
 
 vector <SearchResult> Cinema::SearchShowbylanguage(string userlanguage) {
-    vector<SearchResult> results;
+    vector<SearchResult> result;
     string lower_userlanguage = userlanguage;
-    for (int i = 0; i < userlanguage.size(); i++) {
-        lower_userlanguage[i] = tolower(userlanguage[i]);
+    for (char& c : lower_userlanguage) {
+        c = tolower(c);
     }
+
     for (int i = 0; i < halls.size(); i++) {
         for (int j = 0; j < halls[i].shows.size(); j++) {
-            string lower_movie_language = halls[i].shows[j].movie->language;
-            for (int k = 0; k < halls[i].shows[j].movie->language.size(); k++) {
-                lower_movie_language[k] = tolower(halls[i].shows[j].movie->language[k]);
-            }
+            if (halls[i].shows[j].movie->language != "_DELETED_") {
+                string lower_movie_language = halls[i].shows[j].movie->language;
+                for (char& c : lower_movie_language) {
+                    c = tolower(c);
+                }
 
-            if (lower_movie_language == lower_userlanguage) {
-                SearchResult sr;
-                sr.show = &halls[i].shows[j];
-                sr.hallNumber = halls[i].HallNumber;
-                results.push_back(sr);
+                if (lower_movie_language == lower_userlanguage) {
+                    SearchResult sr;
+                    sr.show = &halls[i].shows[j];
+                    sr.hallNumber = halls[i].HallNumber;
+                    result.push_back(sr);
+                }
             }
         }
     }
-    return results;
+    return result;
 }
 
 vector <SearchResult> Cinema::SearchShowbygenre(string usergenre) {
-    vector<SearchResult> results;
+    vector<SearchResult> result;
     string lower_usergenre = usergenre;
-    for (int i = 0; i < usergenre.size(); i++) {
-        lower_usergenre[i] = tolower(usergenre[i]);
+    for (char& c : lower_usergenre) {
+        c = tolower(c);
     }
+
     for (int i = 0; i < halls.size(); i++) {
         for (int j = 0; j < halls[i].shows.size(); j++) {
-            string lower_movie_genre = halls[i].shows[j].movie->genre;
-            for (int k = 0; k < halls[i].shows[j].movie->genre.size(); k++) {
-                lower_movie_genre[k] = tolower(halls[i].shows[j].movie->genre[k]);
-            }
+            if (halls[i].shows[j].movie->genre != "_DELETED_") {
+                string lower_movie_genre = halls[i].shows[j].movie->genre;
+                for (char& c : lower_movie_genre) {
+                    c = tolower(c);
+                }
 
-            if (lower_movie_genre == lower_usergenre) {
-                SearchResult sr;
-                sr.show = &halls[i].shows[j];
-                sr.hallNumber = halls[i].HallNumber;
-                results.push_back(sr);
+                if (lower_movie_genre == lower_usergenre) {
+                    SearchResult sr;
+                    sr.show = &halls[i].shows[j];
+                    sr.hallNumber = halls[i].HallNumber;
+                    result.push_back(sr);
+                }
             }
         }
     }
-    return results;
+    return result;
 }
 
 
 vector <SearchResult> Cinema::SearchShowbyreleasedate(string userreleasedate) {
-    vector<SearchResult> results;
+    vector<SearchResult> result;
     string lower_userreleasedate = userreleasedate;
-    for (int i = 0; i < userreleasedate.size(); i++) {
-        lower_userreleasedate[i] = tolower(userreleasedate[i]);
+    for (char& c : lower_userreleasedate) {
+        c = tolower(c);
     }
+
     for (int i = 0; i < halls.size(); i++) {
         for (int j = 0; j < halls[i].shows.size(); j++) {
-            string lower_movie_releasedate = halls[i].shows[j].movie->releasedate;
-            for (int k = 0; k < halls[i].shows[j].movie->releasedate.size(); k++) {
-                lower_movie_releasedate[k] = tolower(halls[i].shows[j].movie->releasedate[k]);
-            }
+            if (halls[i].shows[j].movie->releasedate != "_DELETED_") {
+                string lower_movie_releasedate = halls[i].shows[j].movie->releasedate;
+                for (char& c : lower_movie_releasedate) {
+                    c = tolower(c);
+                }
 
-            if (lower_movie_releasedate == lower_userreleasedate) {
-                SearchResult sr;
-                sr.show = &halls[i].shows[j];
-                sr.hallNumber = halls[i].HallNumber;
-                results.push_back(sr);
+                if (lower_movie_releasedate == lower_userreleasedate) {
+                    SearchResult sr;
+                    sr.show = &halls[i].shows[j];
+                    sr.hallNumber = halls[i].HallNumber;
+                    result.push_back(sr);
+                }
             }
         }
     }
-    return results;
+    return result;
 }
 
 void Cinema::addMovie() {
