6c1a042ba1083d5249f9ce7a4e11ab179b25a21c
AARikova22 <AARikova22@codingburgas.bg>
2025-06-30 13:24:53 +0300

    new interface functions added

--- include/user_interface.h
+++ include/user_interface.h
@@ -2,5 +2,6 @@
 #include "cinema.h"
 
 void adminMenu(Cinema& cinema);
-void userMenu(Cinema& cinema);
 void printTicket(const Cinema& cinema, const SearchResult& searchResult, int seatNumber);
+void searchMenu(Cinema& cinema);
+void processBooking(Cinema& cinema, SearchResult& selectedResult);
